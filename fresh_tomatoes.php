aff8c0e5b8fc17dd45dda5d4d83e6c3f8e96e8ae
Zachariah Nagatani <zachnagatani@gmail.com>
2017-03-24 12:50:27 -0700

    Feat: Pull from mysql instead of hardcoded variables

--- fresh_tomatoes.php
+++ fresh_tomatoes.php
@@ -1,57 +1,74 @@
 <?php
+    include('config.php');
     include('media.php');
 
-    $batman_begins = new Movie('Batman Begins', 'After training with his ' .
-                                    'mentor, Batman begins his fight to free ' .
-                                    'crime-ridden Gotham City from corruption',
-                                    'https://upload.wikimedia.org/wikipedia/en' .
-                                    '/a/af/Batman_Begins_Poster.jpg', 'neY2xVmOfUM');
+    // $sql = "SELECT * FROM movies";
+    // $result = $conn->query($sql);
+    // $movies = array();
+    // if ($result->num_rows > 0) {
+    //     while($row = $result->fetch_assoc()) {
+    //         array_push($movies, $row);
+    //     }
+    // } else {
+    //     echo "0 Results";
+    // }
 
-    $the_dark_knight = new Movie('The Dark Knight', 'Batman faces a new ' .
-                                    'threat in Gotham: the sadistic ' .
-                                    'mastermind, the Joker, who looks to ' .
-                                    'release anarchy upon the city', 'ht' .
-                                    'tps://upload.wikimedia.org/wikipedia/' .
-                                    'en/8/8a/Dark_Knight.jpg', 'EXeTwQWrcwY');
-    $the_dark_knight_rises = new Movie('The Dark Knight Rises', 'Eight ' .
-                                        'years after the Joker\'s reign ' .
-                                        'of anarchy, the Dark Knight, ' .
-                                        'with the help of the enigmatic ' .
-                                        'Selina, is forced from his ' .
-                                        'imposed exile to save Gotham ' .
-                                        'City, now on the edge of total ' .
-                                        'annihilation, from the brutal ' .
-                                        'guerrilla terrorist Bane.',
-                                        'https://upload.wikimedia.org/' .
-                                        'wikipedia/en/8/83/Dark_knight' .
-                                        '_rises_poster.jpg', 'GokKUqLcvD8');
+    // echo $movies;
 
-    # Bourne movies
-    $the_bourne_identity = new Movie('The Bourne Identity', 'A man ' .
-                                        'is picked up by a fishing boat, ' .
-                                        'bullet-riddled and suffering ' .
-                                        'from amnesia, races to elude ' .
-                                        'assassins and regain his memory.',
-                                        'https://upload.wikimedia.org/' .
-                                        'wikipedia/en/a/ae/BourneIdentity' .
-                                        'film.jpg', 'FpKaB5dvQ4g');
-    $the_bourne_supremacy = new Movie('The Bourne Supremacy', 'When ' .
-                                        'Jason Bourne is framed for a ' .
-                                        'CIA operation gone awry, he is ' .
-                                        'forced to resume his former ' .
-                                        'life as a trained assassin to ' .
-                                        'survive.', 'https://upload.' .
-                                        'wikimedia.org/wikipedia/en/3/30/' .
-                                        'Bourne_supremacy_ver2.jpg', 'Y-HqyyfBbSo');
-    $the_bourne_ultimatum = new Movie('The Bourne Ultimatum', 'Jason ' .
-                                        'Bourne dodges a ruthless CIA ' .
-                                        'official and his agents from a ' .
-                                        'new assassination program while ' .
-                                        'searching for the origins of his ' .
-                                        'life as a trained killer.', 'https:' .
-                                        '//upload.wikimedia.org/wikipedia/en' .
-                                        '/f/fe/The_Bourne_Ultimatum_%282007' .
-                                        '_film_poster%29.jpg', 'ZT2ZxjUjSo0');
+    $conn->close();
 
-    $movies = array($batman_begins, $the_dark_knight, $the_dark_knight_rises, $the_bourne_identity, $the_bourne_supremacy, $the_bourne_ultimatum);
+    // $batman_begins = new Movie('Batman Begins', 'After training with his ' .
+    //                                 'mentor, Batman begins his fight to free ' .
+    //                                 'crime-ridden Gotham City from corruption',
+    //                                 'https://upload.wikimedia.org/wikipedia/en' .
+    //                                 '/a/af/Batman_Begins_Poster.jpg', 'neY2xVmOfUM');
+
+    // $the_dark_knight = new Movie('The Dark Knight', 'Batman faces a new ' .
+    //                                 'threat in Gotham: the sadistic ' .
+    //                                 'mastermind, the Joker, who looks to ' .
+    //                                 'release anarchy upon the city', 'ht' .
+    //                                 'tps://upload.wikimedia.org/wikipedia/' .
+    //                                 'en/8/8a/Dark_Knight.jpg', 'EXeTwQWrcwY');
+
+    // $the_dark_knight_rises = new Movie('The Dark Knight Rises', 'Eight ' .
+    //                                     'years after the Joker\'s reign ' .
+    //                                     'of anarchy, the Dark Knight, ' .
+    //                                     'with the help of the enigmatic ' .
+    //                                     'Selina, is forced from his ' .
+    //                                     'imposed exile to save Gotham ' .
+    //                                     'City, now on the edge of total ' .
+    //                                     'annihilation, from the brutal ' .
+    //                                     'guerrilla terrorist Bane.',
+    //                                     'https://upload.wikimedia.org/' .
+    //                                     'wikipedia/en/8/83/Dark_knight' .
+    //                                     '_rises_poster.jpg', 'GokKUqLcvD8');
+
+    // # Bourne movies
+    // $the_bourne_identity = new Movie('The Bourne Identity', 'A man ' .
+    //                                     'is picked up by a fishing boat, ' .
+    //                                     'bullet-riddled and suffering ' .
+    //                                     'from amnesia, races to elude ' .
+    //                                     'assassins and regain his memory.',
+    //                                     'https://upload.wikimedia.org/' .
+    //                                     'wikipedia/en/a/ae/BourneIdentity' .
+    //                                     'film.jpg', 'FpKaB5dvQ4g');
+    // $the_bourne_supremacy = new Movie('The Bourne Supremacy', 'When ' .
+    //                                     'Jason Bourne is framed for a ' .
+    //                                     'CIA operation gone awry, he is ' .
+    //                                     'forced to resume his former ' .
+    //                                     'life as a trained assassin to ' .
+    //                                     'survive.', 'https://upload.' .
+    //                                     'wikimedia.org/wikipedia/en/3/30/' .
+    //                                     'Bourne_supremacy_ver2.jpg', 'Y-HqyyfBbSo');
+    // $the_bourne_ultimatum = new Movie('The Bourne Ultimatum', 'Jason ' .
+    //                                     'Bourne dodges a ruthless CIA ' .
+    //                                     'official and his agents from a ' .
+    //                                     'new assassination program while ' .
+    //                                     'searching for the origins of his ' .
+    //                                     'life as a trained killer.', 'https:' .
+    //                                     '//upload.wikimedia.org/wikipedia/en' .
+    //                                     '/f/fe/The_Bourne_Ultimatum_%282007' .
+    //                                     '_film_poster%29.jpg', 'ZT2ZxjUjSo0');
+
+    // $movies = array($batman_begins, $the_dark_knight, $the_dark_knight_rises, $the_bourne_identity, $the_bourne_supremacy, $the_bourne_ultimatum);
 ?>
--- media.php
+++ media.php
@@ -12,4 +12,17 @@
             $this->yt_id = $yt_id;
         }
     }
+
+    include('config.php');
+    $sql = "SELECT * FROM movies";
+    $result = $conn->query($sql);
+    $movies = array();
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            array_push($movies, new Movie($row['title'], $row['storyline'], $row['poster'], $row['yt_id']));
+            // echo "<br>" . $row['title'] . "<br>" . $row['storyline'] . "<br>" . $row['poster'] . "<br>" . $row['yt_id'];
+        }
+    } else {
+        echo "0 Results";
+    }
 ?>
